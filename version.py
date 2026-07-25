e237d2ebf2b9113ddd18774a87aff78f4a5c1a06
yusen <sanmusen_wu@outlook.com>
2026-07-25 19:43:34 +0800

    0.4.2

--- version.py
+++ version.py
@@ -1,7 +1,16 @@
-version_str = "0.4.1"
+version_str = "0.4.2"
 release_info = """
-# 更新
 
-添加了更新检测功能
+[VRCTTP 使用教程-哔哩哔哩](https://www.bilibili.com/video/BV1cgjN6fEfm/)
+
+如果你是第一次使用，完整程序包下载请去 [夸克网盘](https://pan.quark.cn/s/232eeac3d38b)
+
+如果你想要更新版本，可以在打开程序后的更新弹窗下点击更新按钮。
+
+交流群：964670098
+
+---
+
+添加半透明窗口以支持游戏内 Steamvr 手腕窗口绑定
 
 """
