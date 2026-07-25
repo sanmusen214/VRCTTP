4618ca73eff3979bae94d3b567d0282025019b28
yusen <sanmusen_wu@outlook.com>
2026-07-25 20:25:51 +0800

    version text

--- version.py
+++ version.py
@@ -1,11 +1,10 @@
 version_str = "0.4.3"
 release_info = """
-
+目前更新逻辑有点问题，请先关闭软件本体后，双击VRCTTP_UPDATE.exe 进行更新
+---
 [VRCTTP 使用教程-哔哩哔哩](https://www.bilibili.com/video/BV1cgjN6fEfm/)
 
 如果你是第一次使用，完整程序包下载请去 [夸克网盘](https://pan.quark.cn/s/232eeac3d38b)
-
-如果你想要更新版本，可以在打开程序后的更新弹窗下点击更新按钮。
 
 交流群：964670098
 
